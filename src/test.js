77d5b419a13d29a1f6d1450f0265cc898ce019cf
Glenn Hinks <ghinks@yahoo.com>
2018-12-25 16:57:20 -0500

    try suggestion

--- src/test.js
+++ src/test.js
@@ -1,8 +1,10 @@
 import { MyDiv } from './App';
+import renderer from 'react-test-renderer'
+import React from 'react'
 
 describe('Test Component using hooks', () => {
   test('MyDiv', () => {
-    const div = MyDiv();
+    const div = renderer.create(<MyDiv/>).toJSON();
     expect(div).toMatchSnapshot();
   })
 
